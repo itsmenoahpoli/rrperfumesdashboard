d091b2991a48ead5ddc3335c44815fc594a0a8ea
patrickpoli <patrickpolicarpio08@gmail.com>
2022-06-15 06:03:38 +0800

    hid dashbaord button in sidebar

--- src/routes/index.js
+++ src/routes/index.js
@@ -41,15 +41,15 @@ const getUserRole = () => {
 };
 
 export const appRoutes = [
-  {
-    path: "/",
-    component: <Navigate to="/dashboard" replace />,
-    meta: {
-      name: "Dashboard",
-      auth: true,
-      sidebarItem: false,
-    },
-  },
+  // {
+  //   path: "/",
+  //   component: <Navigate to="/dashboard" replace />,
+  //   meta: {
+  //     name: "Dashboard",
+  //     auth: true,
+  //     sidebarItem: false,
+  //   },
+  // },
   {
     path: "/login",
     name: "auth-login",
